7608b6a29b3b9fe0ae54045c45d102b8396673e7
NicholasJardine <nicholasjardine0@gmail.com>
2024-09-05 15:33:40 +0200

    added first header

---
+++ src/sections/Header.tsx
@@ -0,0 +1,10 @@
+import ArrowRight from "@/assets/arrow-right.svg"
+
+export const Header = () => {
+  return <div className="flex justify-center items-center py-3 bg-black text-white">
+    <div className="inline-flex gap-1 items-center">
+    <p>Get started for free</p>
+    <ArrowRight className='h-4 w-4 inline-flex justify-center items-center'/>
+    </div>
+  </div>;
+};
